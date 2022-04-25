f6635416647bd82971ed0f528c00dc7aca3d7db2
Hutr0 <Hutro-games@yandex.ru>
2022-04-26 00:09:35 +0300

    Creating the application

---
+++ Local positioning system/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Local positioning system
+//
+//  Created by Леонид Лукашевич on 25.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
