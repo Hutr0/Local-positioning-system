d68dd99a2ebd4b82cd5165b8bd3bd802c3d65639
Hutr0 <Hutro-games@yandex.ru>
2022-04-28 02:17:36 +0300

    checkGettingInsideArea was realized

--- Local positioning system/Manager/MapManager.swift
+++ Local positioning system/Manager/MapManager.swift
@@ -10,12 +10,46 @@ import CoreLocation
 
 
 class MapManager {
-    var buildingCoordinate = BuildingCoordinate()
-    var buildingArea = BuildingArea()
+    lazy var buildingCoordinate = BuildingCoordinate()
+    lazy var buildingArea = BuildingArea()
     
     let locationServicesManager = LocationServicesManager()
+    let timerManager = TimerManager()
     
     func setupLocationServices() {
         locationServicesManager.checkLocationServices()
+    }
+    
+    func startDetectionGettingInsideArea() {
+        timerManager.startTimer(timeInterval: 10) {
+            guard let userLocation = self.locationServicesManager.locationManager.location?.coordinate else { return }
+            let inside = self.checkGettingInsideArea(userLocation: userLocation)
+
+            if !inside { return }
+            
+            
+        }
+    }
+    
+    func checkGettingInsideArea(userLocation: CLLocationCoordinate2D) -> Bool {
+        
+        let lb = buildingArea.leftBottom
+        let lt = buildingArea.leftTop
+        let rb = buildingArea.rightBottom
+        let rt = buildingArea.rightTop
+        
+        var isInside = false
+        if userLocation.longitude >= lb.longitude &&
+            userLocation.longitude <= rt.longitude &&
+            userLocation.latitude <= lt.latitude &&
+            userLocation.latitude >= rb.latitude {
+            isInside = true
+        }
+        
+        return isInside
+    }
+    
+    func stopDetectionGettingInsideArea() {
+        timerManager.stopTimer()
     }
 }
--- Local positioning systemTests/Manager/MapManagerTests.swift
+++ Local positioning systemTests/Manager/MapManagerTests.swift
@@ -7,6 +7,7 @@
 
 import XCTest
 @testable import Local_positioning_system
+import CoreLocation
 
 class MapManagerTests: XCTestCase {
     
@@ -30,5 +31,47 @@ class MapManagerTests: XCTestCase {
     
     func testLocationServicesManagerNotNil() {
         XCTAssertNotNil(sut.locationServicesManager)
+    }
+    
+    func testTimerManagerManagerNotNil() {
+        XCTAssertNotNil(sut.timerManager)
+    }
+    
+    func testStartDetectionGettingInsideAreaStartsTimer() {
+        sut.startDetectionGettingInsideArea()
+        
+        XCTAssertNotNil(sut.timerManager.timer)
+        XCTAssertTrue(sut.timerManager.timer.isValid)
+    }
+    
+    func testStopDetectionGettingInsideAreaStopTimer() {
+        sut.startDetectionGettingInsideArea()
+        sut.stopDetectionGettingInsideArea()
+        
+        XCTAssertFalse(sut.timerManager.timer.isValid)
+    }
+    
+    func testCheckGettingInsideAreaWorksCorrectlyFirstTest() {
+        let location = CLLocationCoordinate2D(latitude: 55.67239263212078, longitude: 37.47897356390552)
+        
+        let result = sut.checkGettingInsideArea(userLocation: location)
+        
+        XCTAssertTrue(result)
+    }
+    
+    func testCheckGettingInsideAreaWorksCorrectlySecondTest() {
+        let location = CLLocationCoordinate2D(latitude: 55.671592512248566, longitude: 37.47835052742039)
+        
+        let result = sut.checkGettingInsideArea(userLocation: location)
+        
+        XCTAssertFalse(result)
+    }
+    
+    func testCheckGettingInsideAreaWorksCorrectlyThirdTest() {
+        let location = CLLocationCoordinate2D(latitude: 55.67239761556922, longitude: 37.478390897967074)
+        
+        let result = sut.checkGettingInsideArea(userLocation: location)
+        
+        XCTAssertTrue(result)
     }
 }
